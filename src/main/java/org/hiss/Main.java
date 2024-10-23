a437bb227f5ce12c2ca1580cf43c261803455c39
hiss <hissoffc@gmail.com>
2024-10-24 01:01:42 +0500

    Добавил подключение к бд, сущность и сохранение её в базу данных

--- src/main/java/org/hiss/Main.java
+++ src/main/java/org/hiss/Main.java
@@ -1,7 +1,33 @@
 package org.hiss;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hiss.entity.User;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            // Создаём сущность
+            User user = User.builder()
+                    .username("hiss@gmail.com")
+                    .age(22)
+                    .firstname("Hiss")
+                    .birthDate(LocalDate.of(2002, 10, 18))
+                    .lastname("Dev")
+                    .build();
+            // Сохраняем её
+            session.save(user);
+            //Чтобы отправлять запросы в бд, нужно делать коммит вручную
+            session.getTransaction().commit();
+        }
     }
 }
---
+++ src/main/java/org/hiss/entity/User.java
@@ -0,0 +1,46 @@
+package org.hiss.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "users", schema = "public")
+public class User {
+
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    private Integer age;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getUsername() != null && Objects.equals(getUsername(), user.getUsername());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
